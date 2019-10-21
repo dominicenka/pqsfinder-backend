5ab08ac1149cc67959a9f2afd1374510ce96a834
Jiri Hon <jiri.hon@gmail.com>
2019-10-21 15:12:06 +0200

    added support for creating pool of pqsfinder backend workers

--- run.R
+++ run.R
@@ -1,4 +1,10 @@
-library(pqsfinder)
 library(plumber)
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 1) {
+  stop("run.R <instance>")
+}
+instance_port <- 8000 + as.integer(args[[1]])
+
 r <- plumb("../pqsfinder-backend/plumber.R")
-r$run(port=8000)
+r$run(port = instance_port)
